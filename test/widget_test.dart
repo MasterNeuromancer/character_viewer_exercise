469af1427de3939e3e37515ad3ac7768735831d4
“Neuromancer93” <itrejonumber1@gmail.com>
2023-08-20 21:58:45 -0700

    started widgets tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,32 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:character_viewer_exercise/character_viewer_app.dart';
+import 'package:character_viewer_exercise/flavor_config.dart';
+import 'package:character_viewer_exercise/run_app_with_flavor.dart'
+    show flavorConfigProvider;
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Tests Character Viewer App', (WidgetTester tester) async {
+    FlavorConfig config = FlavorConfig()
+      ..appTitle = "The Simpsons Character Viewer"
+      ..apiEndpoint =
+          "http://api.duckduckgo.com/?q=simpsons+characters&format=json"
+      ..imageLocation = "assets/images/the_simpson_no_image.jpeg"
+      ..theme = ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        useMaterial3: true,
+      );
+    flavorConfigProvider = StateProvider((ref) => config);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CharacterViewerApp());
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: CharacterViewerApp(),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(TextFormField), findsOneWidget);
   });
 }
